b2e7cca916e89a467777850a8835d707adbe58a7
Usman <usman.mughal@outlook.fr>
2025-03-17 19:09:21 +0100

    fix: Version initial du bot

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 from threading import Thread
+from database import supabase  # Importez le client Supabase
 
 app = Flask('')
 
 @app.route('/')
 def home():
     return "Bot is alive!"
+
+@app.route('/votes')
+def get_votes():
+    try:
+        result = supabase.table("votes").select("*").execute()
+        return jsonify(result.data)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 def run():
     app.run(host='0.0.0.0', port=8080)
